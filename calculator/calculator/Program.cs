3f0ac60fc73c203aaf3e9c841cb517965400c130
Mahmoud khaled <164625524+Mahmoudkhaled17@users.noreply.github.com>
2025-12-13 01:07:59 +0200

    Add files via upload

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first number");
+            string firstInput = Console.ReadLine();
+            Console.WriteLine("second number");
+            string secondInput = Console.ReadLine();
+            Console.WriteLine("type of operation (+, -, *, /)");
+            string operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"Result: {Convert.ToDouble(firstInput) + Convert.ToDouble(secondInput)}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Result: {Convert.ToDouble(firstInput) - Convert.ToDouble(secondInput)}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Result: {Convert.ToDouble(firstInput) * Convert.ToDouble(secondInput)}");
+                    break;
+                case "/":
+                    Console.WriteLine($"Result: {Convert.ToDouble(firstInput) / Convert.ToDouble(secondInput)}");
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+
+            }
+        }
+    }
+}
